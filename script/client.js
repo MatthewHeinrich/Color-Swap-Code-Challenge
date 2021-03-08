1da891234640b03836ef858d20b5dee9dd92fc93
MatthewHeinrich <mheinrich95@gmail.com>
2021-03-08 14:42:21 -0600

    color change to yellow, still a few kinks to work out

--- script/client.js
+++ script/client.js
@@ -4,10 +4,19 @@ let count = 0;
 
 function onReady() {
     $('#generateButton').on('click', generateColorSwap);
+    $('.divIn').on('click', '.deleteButton', function() {
+        $(this).closest('div').remove();
+    })
+    $('.divIn').on('click', '.colorChange', function() {
+        $(this).closest('div').css({'background-color': 'yellow'})
+    })
 }
 
 function generateColorSwap() {
-    count++;
-    $('.divIn').append(count);
-    $('.divIn').append('<div><button>Yellow</button><button>Delete</button></div>');
+    $('.divIn').append('<div class="divOut"><p class="counter"></p><button class="colorChange">Yellow</button><button class="deleteButton">Delete</button></div>');
+    $('.counter').append(count++);
 }
+
+// function colorChanger() {
+//     $(this).css({'background-color': 'yellow'});
+// }
